4065f0226888f593c14f5a9115dc6985f5023036
櫻田亮太 <tns.r.14xx@gmail.com>
2021-07-03 18:14:26 +0900

    環境構築完了

--- souce/routes/web.php
+++ souce/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/redis_test', function () {
+    Redis::set("hoge", "iii");
+
+    print_r(Redis::get("hoge"));
 });
