46987f714472c3c7ae5d5c9e4cd05048992787ef
櫻田亮太 <tns.r.14xx@gmail.com>
2021-07-25 17:56:10 +0900

    支出テーブル作成

---
+++ source/database/migrations/2021_07_25_085303_create_incomes_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIncomesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('incomes', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('room_id')->unsigned();
+            $table->bigInteger('user_id')->unsigned();
+            $table->bigInteger('category_id')->unsigned();
+            $table->bigInteger('income');
+            $table->text('comment')->nullable();
+            $table->integer('repetition_flg');
+            $table->dateTime('regist_date');
+            $table->integer('del_flg');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('room_id')->references('room_id')->on('rooms')->onDelete('cascade');
+            $table->foreign('category_id')->references('category_id')->on('incomes_categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('incomes');
+    }
+}
