d1a307ac69643c1b07ff04e69025eac4ab3ba94a
櫻田亮太 <tns.r.14xx@gmail.com>
2021-11-24 00:12:39 +0900

    no message

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,6 +1,10 @@
 import axios from "axios";
 
+// export const apiClient = axios.create({
+//   baseURL: "http://localhost:8080/api",
+//   withCredentials: true,
+// });
 export const apiClient = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "http://35.75.50.32/api",
   withCredentials: true,
 });
