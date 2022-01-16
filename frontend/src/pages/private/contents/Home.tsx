b7795b0717f8f5227c97492c5dc97c9caa1383bd
櫻田亮太 <tns.r.14xx@gmail.com>
2022-01-16 18:01:41 +0900

    バグ修正

--- frontend/src/pages/private/contents/Home.tsx
+++ frontend/src/pages/private/contents/Home.tsx
@@ -145,7 +145,7 @@ const Home: React.FC<HomeProps> = ({ roomName }) => {
                   )}
                 </div>
               </div>
-              <div className=" w-2/5 md:w-full">
+              <div className=" w-2/5 md:w-full lg:flex lg:justify-between">
                 {balance && budget ? (
                   budget - balance.data.expense.total > 0 ? (
                     <div className=" md:flex md:justify-between">
